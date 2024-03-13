eba47f376785309c73bd00ee1cb78226e22409cd
Walter Kimaro <wakyj07@gmail.com>
2024-03-13 12:37:15 +0300

    Use dynamic tailwind processor at run time, add support for container queries

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import "./index.css";
+// import "./index.css";
 
 import "./init";
 
