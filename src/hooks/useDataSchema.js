6aaf61239e73b4330e1d3c529dbc8b14b204e7c0
Walter Kimaro <wakyj07@gmail.com>
2024-03-23 21:13:37 +0300

    Setup data schema component

---
+++ src/hooks/useDataSchema.js
@@ -0,0 +1,31 @@
+import useLocalStorageState from "./useLocalStorageState";
+
+export default function useDataSchema(
+	key,
+	defaultValue,
+	mainCallback = () => {}
+) {
+	const keySet = typeof key == "string";
+	if (!keySet) defaultValue = key;
+
+	const [data, setData] = useLocalStorageState(
+		keySet ? key : null,
+		defaultValue || {}
+	);
+	const updateField = (field, newValue, callback) => {
+		const keyValueUpdate = typeof field == "string";
+		callback = (keyValueUpdate ? callback : newValue) || mainCallback;
+
+		const updatedProps = keyValueUpdate ? { [field]: newValue } : field;
+		const newData = { ...data, ...updatedProps };
+
+		handleSetData(newData, callback);
+	};
+
+	const handleSetData = (data, callback = mainCallback) => {
+		setData(data);
+		callback(data);
+	};
+
+	return [data, updateField, handleSetData];
+}
