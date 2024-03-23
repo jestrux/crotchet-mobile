fc55e50e3a1adb1fc6d0e76748b029a309ad0bbc
Walter Kimaro <wakyj07@gmail.com>
2024-03-23 16:16:39 +0300

    Only use capacitor keyboard when available on platform

--- src/hooks/useKeyboard.jsx
+++ src/hooks/useKeyboard.jsx
@@ -1,10 +1,41 @@
-import { Keyboard } from "@capacitor/keyboard";
-import { useEffect, useState } from "react";
+import { Capacitor } from "@capacitor/core";
+import { useEffect, useRef, useState } from "react";
+
+const getKeyboard = async () => {
+	if (window.CapacitorKeyboard) return window.CapacitorKeyboard;
+
+	if (!Capacitor.isPluginAvailable("Keyboard")) return;
+
+	return (await import("@capacitor/keyboard")).Keyboard;
+};
+
+getKeyboard();
 
 export default function useKeyboard() {
 	const [keyboardHeight, setKeyboardHeight] = useState(0);
+	let keyboardPlugin = useRef();
 
 	useEffect(() => {
+		initialize();
+		return cleanKeyboardListeners;
+	});
+
+	const cleanKeyboardListeners = async () => {
+		try {
+			if (typeof keyboardPlugin.current?.removeAllListeners == "function")
+				keyboardPlugin.current.removeAllListeners();
+		} catch (error) {
+			//
+		}
+	};
+
+	const initialize = async () => {
+		const Keyboard = await getKeyboard();
+
+		if (!Keyboard) return;
+
+		keyboardPlugin.current = Keyboard;
+
 		try {
 			Keyboard.setAccessoryBarVisible({
 				isVisible: false,
@@ -24,18 +55,13 @@ export default function useKeyboard() {
 		} catch (error) {
 			//
 		}
-
-		return () => {
-			Keyboard.removeAllListeners();
-		};
-	}, []);
+	};
 
 	const KeyboardPlaceholder = () => (
 		<div
 			className="h-16"
 			style={{
 				height: `${keyboardHeight}px`,
-				// marginTop: "env(safe-area-inset-top)",
 				marginBottom: "env(safe-area-inset-bottom)",
 			}}
 		>
