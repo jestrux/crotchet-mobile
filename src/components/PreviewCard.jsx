6341522f2f0d6d715698fc4c2f4ec000026cd8db
Walter Kimaro <wakyj07@gmail.com>
2024-04-17 20:11:36 +0300

    Add support for icons to preview cards

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -1,15 +1,24 @@
 import clsx from "clsx";
 
-export default function PreviewCard({ image, title, description }) {
+export default function PreviewCard({ icon, image, title, description }) {
 	return (
 		<div className="flex items-center gap-3">
-			{image?.length > 0 && (
-				<div
-					className="border border-content/10 flex-shrink-0 h-10 w-14 bg-content/5 rounded-md bg-cover bg-center"
-					style={{
-						backgroundImage: `url(${image})`,
-					}}
-				></div>
+			{icon?.length ? (
+				<div className="-mr-0.5 bg-content/5 border border-content/5 rounded-lg size-10 flex items-center justify-center">
+					<div
+						className="size-5 flex items-center justify-center"
+						dangerouslySetInnerHTML={{ __html: icon }}
+					></div>
+				</div>
+			) : (
+				image?.length > 0 && (
+					<div
+						className="border border-content/10 flex-shrink-0 h-10 w-14 bg-content/5 rounded-md bg-cover bg-center"
+						style={{
+							backgroundImage: `url(${image})`,
+						}}
+					></div>
+				)
 			)}
 
 			<div className="flex-1">
