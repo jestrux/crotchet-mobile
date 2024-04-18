b9053fe3efb7c5ca678aa85ec5000dc9af6bf21f
Walter Kimaro <wakyj07@gmail.com>
2024-04-18 19:00:48 +0300

    Create multi gesture button that listens to click, long press and double click

---
+++ src/components/MutliGestureButton.jsx
@@ -0,0 +1,44 @@
+import useDoubleClick from "@/hooks/useDoubleClick";
+import { useLongPress } from "@/hooks/useLongPress";
+import { Haptics, ImpactStyle } from "@capacitor/haptics";
+import { useRef } from "react";
+
+export default function MutliGestureButton({
+	onClick = () => {},
+	onDoubleClick,
+	onHold,
+	children,
+	...props
+}) {
+	const recentlyHeld = useRef(false);
+	const { handleClick, handleDoubleClick } = useDoubleClick({
+		onClick,
+		onDoubleClick,
+	});
+	const gestures = useLongPress(() => {
+		if (!_.isFunction(onHold)) return;
+
+		recentlyHeld.current = true;
+
+		Haptics.impact({ style: ImpactStyle.Medium });
+
+		onHold();
+	});
+
+	return (
+		<button
+			{...gestures}
+			onClick={() => {
+				if (recentlyHeld.current) return (recentlyHeld.current = false);
+
+				if (!_.isFunction(onDoubleClick)) return onClick();
+
+				handleClick();
+			}}
+			onDoubleClick={handleDoubleClick}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
