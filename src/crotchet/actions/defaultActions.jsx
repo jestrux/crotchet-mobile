a20073cec268b35cbd34a111c8972ccbad12c94e
Walter Kimaro <wakyj07@gmail.com>
2024-05-31 05:48:55 +0300

    Flesh out send email action

--- src/crotchet/actions/defaultActions.jsx
+++ src/crotchet/actions/defaultActions.jsx
@@ -64,6 +64,60 @@ export const share = {
 	handler: async (payload, { openShareSheet }) => openShareSheet(payload),
 };
 
+export const sendEmail = {
+	global: true,
+	icon: (
+		<svg viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+			/>
+		</svg>
+	),
+	handler: async (payload, { openForm }) => {
+		var res = await openForm({
+			title: "Send Email",
+			data: {
+				subject: "Crotchet Mail",
+				to: "wakyj07@gmail.com",
+				message: "Howdy Partner!",
+				...(payload || {}),
+			},
+			fields: {
+				to: "email",
+				subject: "text",
+				message: "text",
+			},
+		});
+
+		if (!res) return;
+
+		try {
+			await fetch(
+				"https://us-central1-letterplace-c103c.cloudfunctions.net/api/mailer",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						to: res.to,
+						message: {
+							subject: res.subject,
+							text: res.message,
+						},
+					}),
+				}
+			);
+
+			showToast(`Email sent to ${res.to}`);
+		} catch (error) {
+			showToast("Email not sent: " + error);
+		}
+	},
+};
+
 export const clipboard = {
 	icon: (
 		<svg
