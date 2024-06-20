fdd3eb0c647640b1302a15ed9f765219bf8ba4c6
Walter Kimaro <wakyj07@gmail.com>
2024-06-20 12:01:54 +0300

    Add video option to preview card

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 export default function PreviewCard({
 	icon,
 	image,
+	video,
 	title,
 	subtitle,
 	description,
@@ -17,13 +18,32 @@ export default function PreviewCard({
 					></div>
 				</div>
 			) : (
-				image?.length > 0 && (
+				(video || image)?.length > 0 && (
 					<div
-						className="border border-content/10 flex-shrink-0 h-10 w-14 bg-content/5 rounded-md bg-cover bg-center"
+						className="border border-content/10 flex-shrink-0 h-10 w-14 bg-content/5 rounded-md bg-cover bg-center relative"
 						style={{
-							backgroundImage: `url(${image})`,
+							backgroundImage: `url(${video || image})`,
 						}}
-					></div>
+					>
+						{video && (
+							<div className="absolute inset-0 flex items-center justify-center bg-black/30 dark:bg-black/50">
+								<div className="relative size-6 bg-card flex items-center justify-center rounded-full overflow-hidden">
+									<div className="absolute inset-0 bg-content/70"></div>
+									<svg
+										className="size-3.5 ml-0.5 relative text-canvas"
+										viewBox="0 0 24 24"
+										fill="currentColor"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+										/>
+									</svg>
+								</div>
+							</div>
+						)}
+					</div>
 				)
 			)}
 
