390aa3578dc2c9528e35531b34b7ca6332297862
Walter Kimaro <wakyj07@gmail.com>
2024-06-20 12:38:56 +0300

    Add large option to preview card component

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -7,7 +7,76 @@ export default function PreviewCard({
 	title,
 	subtitle,
 	description,
+	large = false,
 }) {
+	if (large) {
+		return (
+			<div
+				className={clsx(
+					"min-h-full w-full flex flex-col gap-2",
+					icon?.length > 0 ? "items-center" : "items-start"
+				)}
+			>
+				{icon?.length ? (
+					<div
+						className="flex items-center justify-center"
+						dangerouslySetInnerHTML={{ __html: icon }}
+					/>
+				) : (
+					<div className="relative flex-shrink-0 bg-content/10 border border-stroke rounded overflow-hidden w-full aspect-[16/9] max-h-40">
+						{(image?.length || video?.length) && (
+							<>
+								<img
+									className={
+										"absolute size-full object-cover"
+									}
+									src={image?.length ? image : video}
+									alt=""
+								/>
+								{video?.length && (
+									<div className="absolute inset-0 bg-black/50 flex items-center justify-center">
+										<div className="relative w-12 h-12 flex items-center justify-center rounded-full overflow-hidden bg-card">
+											<div className="absolute inset-0 bg-content/60"></div>
+											<svg
+												className="w-6 ml-0.5 relative text-canvas"
+												viewBox="0 0 24 24"
+												fill="currentColor"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+												/>
+											</svg>
+										</div>
+									</div>
+								)}
+							</>
+						)}
+					</div>
+				)}
+
+				<div
+					className={clsx(
+						"flex flex-col gap-1 flex-1 min-w-0 space-y-1 px-2 pt-1 pb-3.5",
+						icon?.length && "text-center"
+					)}
+				>
+					{title?.length > 0 && (
+						<h5 className="text-base/none line-clamp-1 font-semibold first-letter:capitalize">
+							{title}
+						</h5>
+					)}
+					{subtitle?.toString().length > 0 && (
+						<p className="text-base/none line-clamp-1 opacity-70">
+							{subtitle}
+						</p>
+					)}
+				</div>
+			</div>
+		);
+	}
+
 	return (
 		<div className="flex items-center gap-3">
 			{icon?.length ? (
